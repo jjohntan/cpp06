1749e7ff3ffd2e5f7c27334643938ea5d52e684f
Jek John Tan <jetan@u82z05s06.42kl.edu.my>
2025-06-12 20:25:18 +0800

    add SC class

---
+++ ex00/ScalarConverter.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/06 16:59:23 by jetan             #+#    #+#             */
+/*   Updated: 2025/06/10 15:32:58 by jetan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER
+# define SCALARCONVERTER
+
+class ScalarConverter
+{
+	private:
+		
+	public:
+		ScalarConverter();
+		~ScalarConverter();
+};
+
+#endif
