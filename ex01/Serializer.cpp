9b7af42085327ebdfbc4cd18768518bc0e090bcc
Jek John Tan <jetan@u91z02s01.42kl.edu.my>
2025-06-16 20:27:19 +0800

    implement main()

--- ex01/Serializer.cpp
+++ ex01/Serializer.cpp
@@ -6,18 +6,26 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 17:28:42 by jetan             #+#    #+#             */
-/*   Updated: 2025/06/14 17:59:01 by jetan            ###   ########.fr       */
+/*   Updated: 2025/06/16 17:26:05 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Serializer.hpp"
 
-uintptr_t serialize(Data* ptr)
+/**
+ * @brief pointer to int.
+ * uintptr_t: guaranteed to be of the same size of
+ * that of the pointer in the given system
+ */
+uintptr_t Serializer::serialize(Data* ptr)
 {
 	return reinterpret_cast<uintptr_t>(ptr);
 }
 
-Data* deserialize(uintptr_t raw)
+/**
+ * @brief int to pointer
+ */
+Data* Serializer::deserialize(uintptr_t raw)
 {
 	return reinterpret_cast<Data*>(raw);
 }
---
+++ ex01/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/16 15:26:22 by jetan             #+#    #+#             */
+/*   Updated: 2025/06/16 20:26:55 by jetan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Serializer.hpp"
+
+int main()
+{
+	Data data;
+	Data *data_ptr;
+	
+	data.num = 42;
+	
+	uintptr_t raw = Serializer::serialize(&data);
+	data_ptr = Serializer::deserialize(raw);
+	
+	std::cout << "value of data: " << data.num << std::endl;
+	std::cout << "raw: " << raw << std::endl;
+	std::cout << "address of data: " << &data << std::endl;
+	std::cout << "address of data pointer: " << data_ptr << std::endl;
+	std::cout << "value of data pointer: " << data_ptr->num << std::endl;
+	
+	//compare
+	if (data_ptr == &data)
+		std::cout << "valid" << std::endl;
+	else
+		std::cout << "invalid" << std::endl;
+}
