982624ecb2232aa607b63b051e0d0216044e70be
Jek John Tan <jetan@u90z02s01.42kl.edu.my>
2025-06-17 18:32:16 +0800

    WIP identify

--- ex02/Base.cpp
+++ ex02/Base.cpp
@@ -6,7 +6,7 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 20:47:36 by jetan             #+#    #+#             */
-/*   Updated: 2025/06/17 15:12:07 by jetan            ###   ########.fr       */
+/*   Updated: 2025/06/17 18:31:39 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,36 @@ Base * generate(void)
 
 void identify(Base* p)
 {
-	
+	if ()
+	{
+		std::cout << "A" << std::endl;
+	}
+	else if ()
+	{
+		std::cout << "B" << std::endl;
+	}
+	else if ()
+	{
+		std::cout << "C" << std::endl;
+	}
+	else
+	{
+		std::cout << "No such type" << std::endl;
+	}
 }
 
+/**
+ * reference type can't return a null reference
+ */
 void identify(Base& p)
 {
+	try
+	{
+		
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 	
 }
--- ex02/Base.hpp
+++ ex02/Base.hpp
@@ -6,12 +6,14 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 20:47:33 by jetan             #+#    #+#             */
-/*   Updated: 2025/06/17 15:11:25 by jetan            ###   ########.fr       */
+/*   Updated: 2025/06/17 15:45:38 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BASE_HPP
 # define BASE_HPP
+
+#include <iostream>
 
 class Base
 {
