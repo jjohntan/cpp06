5c7c4f47731c732a57ab2c09336abe89b3aa2e2a
Jek John Tan <jetan@u91z02s02.42kl.edu.my>
2025-06-23 10:47:55 +0800

    add check int to detecttype()

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,25 +6,26 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 16:59:23 by jetan             #+#    #+#             */
-/*   Updated: 2025/06/22 19:43:16 by jetan            ###   ########.fr       */
+/*   Updated: 2025/06/23 10:42:50 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
-bool isNumber(std::string str)
+bool isNumber(std::string literal)
 {
-	for ()
+	for (unsigned int i = 0; i < literal.length(); i++)
 	{
-		if (!isdigit())
+		if (!isdigit(literal[i]))
 			return false;
-		return true;
 	}
+	return true;
 }
 
 void detectType(std::string literal)
 {
 	if (literal.length() == 1)//check the string length is equals to one
 		std::cout << "Char" << std::endl;
-	if ()
+	if (!isNumber(literal))
+		std::cout << "Int" << std::endl;
 }
