791bf43d44770ffbf1485b66f53f612d701724b7
Jek John Tan <jetan@u91z02s02.42kl.edu.my>
2025-06-23 16:20:04 +0800

    add check float to detecttype()

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,13 +6,23 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 16:59:23 by jetan             #+#    #+#             */
-/*   Updated: 2025/06/23 10:42:50 by jetan            ###   ########.fr       */
+/*   Updated: 2025/06/23 16:06:29 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
-bool isNumber(std::string literal)
+// void ScalarConverter::convert(std::string literal)
+// {
+	
+// }
+
+// bool isFloat(std::string literal)
+// {
+	
+// }
+
+static bool isInt(std::string literal)
 {
 	for (unsigned int i = 0; i < literal.length(); i++)
 	{
@@ -24,8 +34,12 @@ bool isNumber(std::string literal)
 
 void detectType(std::string literal)
 {
-	if (literal.length() == 1)//check the string length is equals to one
+	if (literal.length() == 1 && isprint(literal[0]))//check the string length is equals to one
 		std::cout << "Char" << std::endl;
-	if (!isNumber(literal))
+	if (!isInt(literal))
 		std::cout << "Int" << std::endl;
+	if (literal[literal.length() - 1] == 'f')
+		std::cout << "Float" << std::endl;
+	// if ()
+	// 	std::cout << "Double" << std::endl;
 }
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 16:59:20 by jetan             #+#    #+#             */
-/*   Updated: 2025/06/22 19:35:08 by jetan            ###   ########.fr       */
+/*   Updated: 2025/06/23 16:13:26 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define SCALARCONVERTER
 
 #include <iostream>
+#include <cctype>
 
 class ScalarConverter
 {
@@ -23,7 +24,7 @@ class ScalarConverter
 		ScalarConverter(const ScalarConverter &other);
 		ScalarConverter &operator=(const ScalarConverter &other);
 	public:
-		static void convert(std::string);
+		static void convert(std::string literal);
 };
 
 void detectType(std::string literal);
