7e7b46d30a806d85a5e177a055148fbaee99fbf7
Jek John Tan <jetan@u82z01s05.42kl.edu.my>
2025-06-30 17:13:28 +0800

    done

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 15:26:22 by jetan             #+#    #+#             */
-/*   Updated: 2025/06/16 20:26:55 by jetan            ###   ########.fr       */
+/*   Updated: 2025/06/30 17:11:37 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,8 @@ int main()
 	std::cout << "value of data: " << data.num << std::endl;
 	std::cout << "raw: " << raw << std::endl;
 	std::cout << "address of data: " << &data << std::endl;
-	std::cout << "address of data pointer: " << data_ptr << std::endl;
 	std::cout << "value of data pointer: " << data_ptr->num << std::endl;
+	std::cout << "address of data pointer: " << data_ptr << std::endl;
 	
 	//compare
 	if (data_ptr == &data)
